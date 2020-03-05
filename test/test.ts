ee43af2daaa35ec624472f09362de91532c7f868
narumincho <narumincho.starfy@gmail.com>
2020-03-05 17:14:40 +0900

    fix test type and workflow

--- test/test.ts
+++ test/test.ts
@@ -3,36 +3,43 @@ import * as main from "../source/main";
 describe("test", () => {
   it("https://definy.app/ is Home in English", () => {
     expect(main.urlToLanguageAndLocation("https://definy.app/")).toEqual<
-      main.data.Maybe<main.data.LanguageAndLocation>
-    >(
-      main.data.maybeJust<main.data.LanguageAndLocation>({
-        language: "English",
-        location: main.data.locationHome
-      })
-    );
+      main.data.LanguageAndLocation
+    >({
+      language: "English",
+      location: main.data.locationHome
+    });
   });
   it("project url", () => {
     expect(
       main.urlToLanguageAndLocation(
         "https://definy.app/project/580d8d6a54cf43e4452a0bba6694a4ed?hl=ja"
       )
-    ).toEqual<main.data.Maybe<main.data.LanguageAndLocation>>(
-      main.data.maybeJust<main.data.LanguageAndLocation>({
-        language: "Japanese",
-        location: main.data.locationProject(
-          "580d8d6a54cf43e4452a0bba6694a4ed" as main.data.ProjectId
-        )
-      })
-    );
+    ).toEqual<main.data.LanguageAndLocation>({
+      language: "Japanese",
+      location: main.data.locationProject(
+        "580d8d6a54cf43e4452a0bba6694a4ed" as main.data.ProjectId
+      )
+    });
   });
   it("short url", () => {
     expect(main.urlToLanguageAndLocation("https://definy.app")).toEqual<
-      main.data.Maybe<main.data.LanguageAndLocation>
-    >(
-      main.data.maybeJust<main.data.LanguageAndLocation>({
-        language: "English",
-        location: main.data.locationHome
-      })
+      main.data.LanguageAndLocation
+    >({
+      language: "English",
+      location: main.data.locationHome
+    });
+  });
+  it("encode, decode user url", () => {
+    const languageAndLocation: main.data.LanguageAndLocation = {
+      language: "Esperanto",
+      location: main.data.locationUser(
+        "580d8d6a54cf43e4452a0bba6694a4ed" as main.data.UserId
+      )
+    };
+    const url: string = main.languageAndLocationToUrl(languageAndLocation);
+    const decodedLanguageAndLocation: main.data.LanguageAndLocation = main.urlToLanguageAndLocation(
+      url
     );
+    expect(languageAndLocation).toEqual(decodedLanguageAndLocation);
   });
 });
