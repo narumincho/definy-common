b4305928c553c1106e6ede69216c9bf70f70eb1f
narumincho <narumincho.starfy@gmail.com>
2020-04-09 09:47:45 +0900

    add stringToValidComment

--- source/main.ts
+++ source/main.ts
@@ -131,7 +131,7 @@ const accessTokenFromUrl = (hash: string): data.Maybe<data.AccessToken> => {
 };
 
 export const stringToValidUserName = (userName: string): string | null => {
-  const normalized = normalizeString(userName);
+  const normalized = normalizeOneLineString(userName);
   const length = [...normalized].length;
   if (length <= 0 || 50 < length) {
     return null;
@@ -142,7 +142,7 @@ export const stringToValidUserName = (userName: string): string | null => {
 export const stringToValidProjectName = (
   projectName: string
 ): string | null => {
-  const normalized = normalizeString(projectName);
+  const normalized = normalizeOneLineString(projectName);
   const length = [...normalized].length;
   if (length <= 0 || 50 < length) {
     return null;
@@ -151,7 +151,7 @@ export const stringToValidProjectName = (
 };
 
 export const stringToValidIdeaName = (ideaName: string): string | null => {
-  const normalized = normalizeString(ideaName);
+  const normalized = normalizeOneLineString(ideaName);
   const length = [...normalized].length;
   if (length <= 0 || 100 < length) {
     return null;
@@ -159,15 +159,53 @@ export const stringToValidIdeaName = (ideaName: string): string | null => {
   return normalized;
 };
 
-const normalizeString = (text: string): string => {
+export const stringToValidComment = (comment: string): string | null => {
+  const normalized = normalizeMultiLineString(comment);
+  const length = [...normalized].length;
+  if (length <= 0 || 1500 < length) {
+    return null;
+  }
+  return normalized;
+};
+
+/**
+ * NFKCで正規化して, 改行をLFのみにする
+ */
+const normalizeMultiLineString = (text: string): string => {
+  const normalized = text.normalize("NFKC");
+  let result = "";
+  for (const char of normalized) {
+    const codePoint = char.codePointAt(0);
+    // LF
+    if (codePoint === 0x0a) {
+      result += char;
+      continue;
+    }
+    // 制御文字
+    if (
+      codePoint === undefined ||
+      codePoint <= 0x1f ||
+      (0x7f <= codePoint && codePoint <= 0xa0)
+    ) {
+      continue;
+    }
+    result += char;
+  }
+  return result;
+};
+/**
+ * NFKCで正規化して, 先頭末尾の空白をなくし, 空白の連続を1つの空白にまとめ, 改行を取り除く
+ */
+const normalizeOneLineString = (text: string): string => {
   const normalized = text.normalize("NFKC").trim();
   let result = "";
   let beforeSpace = false;
   for (const char of normalized) {
     const codePoint = char.codePointAt(0);
+    // 制御文字
     if (
       codePoint === undefined ||
-      codePoint <= 0x19 ||
+      codePoint <= 0x1f ||
       (0x7f <= codePoint && codePoint <= 0xa0)
     ) {
       continue;
