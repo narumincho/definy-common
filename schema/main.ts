4e0be1dfe6deefe0cc7719a42d609cb5f2195a45
narumincho <narumincho.starfy@gmail.com>
2020-10-23 21:09:50 +0900

    output schema as binary and json

--- schema/main.ts
+++ schema/main.ts
@@ -1,4 +1,5 @@
 import * as definyCore from "../source/main";
+import * as definyCoreData from "../source/data";
 import * as prettier from "prettier";
 import * as typePartMap from "./typePartMap";
 import { promises as fileSystem } from "fs";
@@ -22,4 +23,27 @@ const elmCode = definyCore.generateElmCodeAsString(typePartMap.typePartMap);
 
 fileSystem.writeFile(elmPath, elmCode).then(() => {
   console.log("output Elm code!");
+});
+
+const typePartMapAsIdAndDataList = [
+  ...typePartMap.typePartMap,
+].map(([id, typePart]) => ({ id, data: typePart }));
+
+const typePartMapAsBinary = definyCoreData.List.codec(
+  definyCoreData.IdAndData.codec(
+    definyCoreData.TypePartId.codec,
+    definyCoreData.TypePart.codec
+  )
+).encode(typePartMapAsIdAndDataList);
+
+const typePartMapAsJson = JSON.stringify(typePartMapAsIdAndDataList);
+
+fileSystem
+  .writeFile("schemaBinary", new Uint8Array(typePartMapAsBinary))
+  .then(() => {
+    console.log("output as binary");
+  });
+
+fileSystem.writeFile("schema.json", typePartMapAsJson).then(() => {
+  console.log("output as json");
 });
