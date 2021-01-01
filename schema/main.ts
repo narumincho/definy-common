e71beec0795e6f1b8d0986277578837d895f0e87
narumincho <narumincho.starfy@gmail.com>
2021-01-01 16:57:33 +0900

    fix schema code

--- schema/main.ts
+++ schema/main.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../node_modules/better-typescript-lib/lib.esnext.full.d.ts" />
+
 import * as definyCore from "../source/main";
 import * as prettier from "prettier";
 import * as typePartMap from "./typePartMap";
